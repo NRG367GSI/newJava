00e117ad289ae9836ae2783ef0eb6c8921754209
NRG367GSI <NRG367GSI@gmail.com>
2023-01-27 19:17:25 +0300

    addad Homework_2

--- Experiments/src/main/java/org/example/Main.java
+++ Experiments/src/main/java/org/example/Main.java
@@ -1,9 +1,31 @@
 package org.example;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
 
         String[] name = {"C", "e", "h"};
         System.out.println(String.join("", name));
+    }
+
+    public static void ExpDir() {
+        try {
+            String pathProperty = System.getProperty("user.dir");
+            String fileSep = System.getProperty("file.separator");
+            String pathFile = (pathProperty + fileSep).concat("TEST.txt");
+            File fileTest = new File(pathFile);
+            System.out.println(pathFile);
+            //System.out.println("\n" + fileTest.getAbsolutePath());
+            if (fileTest.createNewFile()) {
+                System.out.println("Файл создан! \n");
+            } else {
+                System.out.println("Файл не создан! \n");
+            }
+        } catch (Exception e) {
+            System.out.println("Вызвана ошибка: " + e);
+        } finally {
+            System.out.println("finally");
+        }
     }
 }
--- Homework_2/src/main/java/Homework_2/Task_2.java
+++ Homework_2/src/main/java/Homework_2/Task_2.java
@@ -6,26 +6,6 @@ import java.io.PrintWriter;
 
 public class Task_2 {
 
-    public static void ExpDir() {
-        try {
-            String pathProperty = System.getProperty("user.dir");
-            String fileSep = System.getProperty("file.separator");
-            String pathFile = (pathProperty + fileSep).concat("TEST.txt");
-            File fileTest = new File(pathFile);
-            System.out.println(pathFile);
-            //System.out.println("\n" + fileTest.getAbsolutePath());
-            if (fileTest.createNewFile()) {
-                System.out.println("Файл создан! \n");
-            } else {
-                System.out.println("Файл не создан! \n");
-            }
-        } catch (Exception e) {
-            System.out.println("Вызвана ошибка: " + e);
-        } finally {
-            System.out.println("finally");
-        }
-    }
-
     public static void writeFile(String text) throws FileNotFoundException {
         try (PrintWriter writer = new PrintWriter("temp.txt")) {
             writer.write(text);
