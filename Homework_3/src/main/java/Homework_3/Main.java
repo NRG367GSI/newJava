9e77e241528a2fb27b20783b1b13b51251e9a221
NRG367GSI <NRG367GSI@gmail.com>
2023-02-01 15:57:13 +0300

    addad Homework_3

---
+++ Homework_3/src/main/java/Homework_3/Main.java
@@ -0,0 +1,39 @@
+package Homework_3;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        WorkInArr sequence = new WorkInArr();
+        sequence.arr = WorkInArr.CreateArr(5,0,10);
+        System.out.println(sequence.arr);
+
+        // Task_1
+        sequence.evenNum = WorkInArr.removeEvenNum(sequence.arr);
+        System.out.println(sequence.evenNum);
+
+        // Task_2
+        WorkInArr seq = new WorkInArr();
+        seq.arr = WorkInArr.CreateArr(10,0,10);
+        System.out.println(seq.arr);
+
+        seq.minArr = WorkInArr.minArr(seq.arr);
+        System.out.printf("min = %d \n", seq.minArr);
+
+        seq.maxArr = WorkInArr.maxArr(seq.arr);
+        System.out.printf("max = %d \n", seq.maxArr);
+
+        WorkInArr.arithmeticMean(sequence.arr);
+
+
+
+
+    }
+
+
+}
+
+
+
+
---
+++ Homework_3/src/main/java/Homework_3/WorkInArr.java
@@ -0,0 +1,44 @@
+package Homework_3;
+
+import java.util.*;
+
+public class WorkInArr {
+
+    public ArrayList<Integer> arr;
+    public ArrayList<Integer> evenNum;
+    public int minArr;
+    public int maxArr;
+    public int arithmeticMean;
+
+    public static ArrayList<Integer> CreateArr(int size, int min, int max) {
+        ArrayList<Integer> arr = new ArrayList<>(size);
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            arr.add(random.nextInt(min, max));
+        }
+        return arr;
+    }
+
+    public  static ArrayList<Integer> removeEvenNum(ArrayList<Integer> arr) {
+        arr.removeIf(i-> (i%2 == 0));
+        return arr;
+    }
+
+    public  static  int minArr(ArrayList<Integer> arr) {
+        return Collections.min(arr);
+    }
+
+    public  static  int maxArr(ArrayList<Integer> arr) {
+
+        return Collections.max(arr);
+    }
+
+    public  static  void outWorkInArr() {
+
+    }
+
+    public static void arithmeticMean(ArrayList<Integer> arr) {
+        double average = arr.stream().mapToInt(args->args).average().getAsDouble();
+        System.out.println("average = " + average);
+    }
+}
