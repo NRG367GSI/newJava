1df661b4a987978e3acc510678a2c152cd746a93
NRG367GSI <NRG367GSI@gmail.com>
2023-02-04 00:31:35 +0300

    addad Homework_4

--- Homework_4/src/main/java/Homework_4/Main.java
+++ Homework_4/src/main/java/Homework_4/Main.java
@@ -1,11 +1,15 @@
 package Homework_4;
 
+
+
 public class Main {
     public static void main(String[] args) {
 
         //System.out.println("Hello world!");
+        Task_1 exp = new Task_1();
+        exp.sequence =
 
-        DataStorageAndProcessing.menu();
-        System.out.println(DataStorageAndProcessing.sequence);
+        //Task_1.menu();
+        //System.out.println(Task_1.sequence);
     }
 }
